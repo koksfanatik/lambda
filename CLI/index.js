6cf5a81d26b4703d6d30140ce86a14cdfc83431c
Shevchenko Denis <shevchenkodenis.code@gmail.com>
2022-12-12 06:07:54 +0200

    done

--- CLI/index.js
+++ CLI/index.js
@@ -2,51 +2,84 @@ const readline = require('node:readline');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-  //prompt: "Put something",
+  
 });
+let result = [];
+let resultSet;
+let options = "What to do with it: \n1-Display words alphabetically\n2-Display numbers from smallest to largest\n3-Display numbers from largest to smallest\n4-Display words by length\n5-Display only unique words\n6-Display only unique values\nexit-to exit\n ";
+const getArrayOfStrings = function (array) {
+  let arrayOfStrings = [];
+
+  for (let key of array) {
+    if (isNaN(+key)) arrayOfStrings.push(key);
+  };
+  return arrayOfStrings;
+
+};
+const getArrayOfNumbers = function (array) {
+  let ArrayOfNumbers = [];
+
+  for (let key of array) {
+    if (!isNaN(+key)) ArrayOfNumbers.push(key);
+  };
+  return ArrayOfNumbers;
+
+};
 
 const sortSli = function () {
   rl.question("Enter multiple words and numbers separated by spaces ", answer => {
     let arrayOfSample = answer.split(" ");
-    console.log(`Received: ${arrayOfSample}`);
+      console.log(`Received: ${arrayOfSample}`);
   
 
-    rl.question("What to do with it ", answer => {
-      switch (answer) {
+    rl.question(options, answers => {
+      
+      switch (answers) {
         case "1":
-          console.log(`Received: ${answer}`);
+          result = getArrayOfStrings(arrayOfSample);
+          result.sort();
+          console.log(`${result}`);
           sortSli();
           break;
         case "2":
-          console.log(`Received: ${answer}`);
+          result = getArrayOfNumbers(arrayOfSample);
+          result.sort((a, b) => a - b);
+          console.log(`${result}`);
           sortSli();
           break;
         case "3":
-          console.log(`Received: ${answer}`);
+          result = getArrayOfNumbers(arrayOfSample);
+          result.sort((a, b) => b - a);
+          console.log(`${result}`);
           sortSli();
           break;
-        default:
+        case "4":
+          result = getArrayOfStrings(arrayOfSample);
+          result.sort((a, b) => a.length - b.length);
+          console.log(`${result}`);
+          sortSli();
           break;
+        case "5":
+          result = getArrayOfStrings(arrayOfSample);
+          resultSet = new Set();
+          for (let key of result) resultSet.add(key);
+          result = Array.from(resultSet);
+          console.log(`${result}`);
+          sortSli();
+          break;
+        case "6":          
+          resultSet = new Set();
+          for (let key of arrayOfSample) resultSet.add(key);
+          result = Array.from(resultSet);
+          console.log(`${result}`);
+          sortSli();
+          break;
+         case "exit":          
+          rl.close();
+          break;
+        
       }
     });
   });
 };
 sortSli();
-
-
-
-
-
-
-
-/*rl.prompt();
-
-rl.on('line', (line) => {
-  line = line.trim();
-  let arrayOfSample = line.split(" ");
-
-  console.log(`Received: ${arrayOfSample}`);
-  rl.setPrompt("davai ewe")
-  rl.prompt();
-});
-*/
